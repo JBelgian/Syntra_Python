7b88084e7e5993627c2cf381d99dc42f2e2fe822
JBelgian <jordy.hanno@gmail.com>
2022-01-31 13:27:49 +0100

    Initial commit

---
+++ Module 2/Les 8 Herhaling/Muzikanten_Gegevens.py
@@ -0,0 +1,7 @@
+muzikanten = [
+              {"naam": "Jef", "instrumenten": ["trompet", "viool", "gitaar"] },
+              {"naam": "Dirk", "instrumenten": ["piano", "saxofoon"]},
+              {"naam": "Bob", "instrumenten": ["orgel", "basgitaar", "drums", "dwarsfluit"]},
+              {"naam": "Henry", "instrumenten": ["piano", "gitaar", "harp"]}
+             ]
+
