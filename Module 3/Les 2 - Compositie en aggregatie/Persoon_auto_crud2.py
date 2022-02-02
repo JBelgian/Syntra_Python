e5b50116a5b1e903a1f7c463a1e8accc93628688
JBelgian <jordy.hanno@gmail.com>
2022-02-02 21:23:28 +0100

    Initial commit

--- Module 3/Les 2 - Compositie en aggregatie/Persoon_auto_crud2.py
+++ Module 3/Les 2 - Compositie en aggregatie/Persoon_auto_crud2.py
@@ -1,18 +1,23 @@
 class Persoon:
     wagens = []
-    def __init__(self,naam, leeftijd):
+
+    def __init__(self, naam, leeftijd):
         self.naam = naam
         self.leeftijd = leeftijd
-    def voeg_auto_toe(self,Auto):
+
+    def voeg_auto_toe(self, Auto):
         self.wagens.append(Auto)
+
     def print_wagens(self):
         if len(self.wagens) == 0:
             print("Deze persoon heeft geen wagens")
         else:
             for x in self.wagens:
                 print(x.toon_auto_info())
+
     def toon_persoons_info(self):
-        return "De persoon heet {} en is {} jaar oud".format(self.naam,self.leeftijd)
+        return "De persoon heet {} en is {} jaar oud".format(self.naam, self.leeftijd)
+
     def wijzig_auto_gegevens(self):
         id = input("geef het id van de wagen")
         for x in self.wagens:
@@ -23,27 +28,31 @@ class Persoon:
 
     def verwijder_wagen_uit_lijst(self):
         id = input("geef het id van de wagen")
-        for x,o in enumerate(self.wagens):
+        for x, o in enumerate(self.wagens):
             if id == o.id:
                 self.wagens.pop(x)
 
 
-
 class Auto:
-    def __init__(self,id,merk,model,bouwjaar):
+    def __init__(self, id, merk, model, bouwjaar):
         self.id = id
         self.merk = merk
         self.model = model
         self.bouwjaar = bouwjaar
 
     def toon_auto_info(self):
-        return("iD {} : De auto is een {} {} van het bouwjaar {}".format(self.id,self.merk,self.model,self.bouwjaar))
+        return (
+            "iD {} : De auto is een {} {} van het bouwjaar {}".format(self.id, self.merk, self.model, self.bouwjaar))
+
     def verander_info(self):
         self.merk = input("geef het merk van de wagen")
         self.model = input("geef het model van de wagen")
         self.bouwjaar = input("geef het bouwjaar van de wagen")
-#data
-p = Persoon("Jan",22)
+
+
+# data
+p = Persoon("Jan", 22)
+
 
 def toon_menu():
     print("Welke handeling wil je uitvoeren")
@@ -54,36 +63,39 @@ def toon_menu():
     print("5: verander auto info")
     print("6: verwijder auto")
 
+
 def toon_persoons_info():
     print(p.toon_persoons_info())
+
+
 def verander_naam_persoon():
     nieuwe_naam = input("geef een nieuwe naam in")
     p.naam = nieuwe_naam
+
+
 def voeg_wagen_toe():
     id = input("geef het ID in")
     merk = input("geef het merk van de wagen")
     model = input("geef het model van de wagen")
     bouwjaar = input("geef het bouwjaar van de wagen in")
-    wagen = Auto(id,merk,model,bouwjaar)
+    wagen = Auto(id, merk, model, bouwjaar)
     p.voeg_auto_toe(wagen)
-
 
 
 # hoofdprogramma
 toon_menu()
 keuze = input("geef je keuze in:")
-while(not keuze == "stop"):
-    if(keuze == "1"):
+while not keuze == "stop":
+    if keuze == "1":
         toon_persoons_info()
-    elif(keuze == "2"):
+    elif keuze == "2":
         verander_naam_persoon()
-    elif(keuze == "3"):
+    elif keuze == "3":
         p.print_wagens()
-    elif(keuze == "4"):
+    elif keuze == "4":
         voeg_wagen_toe()
-    elif(keuze == "5"):
+    elif keuze == "5":
         p.wijzig_auto_gegevens()
-    elif(keuze == "6"):
+    elif keuze == "6":
         p.verwijder_wagen_uit_lijst()
     keuze = input("geef je keuze in:")
-
--- Module 3/Les 2 - Compositie en aggregatie/Studenten.py
+++ Module 3/Les 2 - Compositie en aggregatie/Studenten.py
@@ -1,4 +1,5 @@
-import Student as stud
+import Student as Stud
+
 
 def toon_menu():
     print("1: toon lijst met studenten")
@@ -7,53 +8,55 @@ def toon_menu():
     print("4: voeg student toe")
     print("5: verwijder student")
 
+
 def toon_lijst_studenten():
     print("Lijst met studenten")
     print("--------------------------")
-    for x in stud.lijst_studenten:
+    for x in Stud.lijst_studenten:
         print(x.toon_student_info())
+
 
 def verander_student_naam():
     zoek_naam = input("geef de naam in van de gezochte student")
-    for x in stud.lijst_studenten:
+    for x in Stud.lijst_studenten:
         if x.naam == zoek_naam:
             nieuwe_naam = input("geef een nieuwe naam voor de student")
             x.verander_naam(nieuwe_naam)
 
+
 def verander_student_richting():
     zoek_naam = input("geef de naam in van de gezochte student")
-    for x in stud.lijst_studenten:
+    for x in Stud.lijst_studenten:
         if x.naam == zoek_naam:
             nieuwe_richting = input("geef een nieuwe studierichting in")
             x.verander_richting(nieuwe_richting)
 
+
 def voeg_student_toe():
     naam = input("geef de naam van de student")
     studierichting = input("Welke studierichting volgt de studenten")
-    stud.lijst_studenten.append(stud.Student(naam,studierichting))
+    Stud.lijst_studenten.append(Stud.Student(naam, studierichting))
+
 
 def verwijder_student():
     zoek_naam = input("geef de naam in van de gezochte student")
-    for x,o in enumerate(stud.lijst_studenten):
+    for x, o in enumerate(Stud.lijst_studenten):
         if o.naam == zoek_naam:
-            stud.lijst_studenten.pop(x)
-
+            Stud.lijst_studenten.pop(x)
 
 
 toon_menu()
 keuze = input("geef je keuze in:")
-while(not keuze == "stop"):
-    if(keuze == "1"):
+while not keuze == "stop":
+    if keuze == "1":
         toon_lijst_studenten()
-    elif(keuze == "2"):
+    elif keuze == "2":
         verander_student_naam()
-    elif(keuze == "3"):
+    elif keuze == "3":
         verander_student_richting()
-    elif(keuze == "4"):
+    elif keuze == "4":
         voeg_student_toe()
-    elif(keuze == "5"):
+    elif keuze == "5":
         verwijder_student()
 
     keuze = input("geef je keuze in:")
-
-
