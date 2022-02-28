bddf3f7569a186b08411328c3a71c0555505c046
JBelgian <jordy.hanno@gmail.com>
2022-02-28 13:45:09 +0100

    Commit

--- Module 3/Les 6/Bibliotheek.py
+++ Module 3/Les 6/Bibliotheek.py
@@ -1,36 +1,24 @@
 class Boek:
-    def __init__(self, id, titel, auteur):
+    def __init__(self, id, naam, auteur):
         self.id = id
-        self.titel = titel
+        self.naam = naam
         self.auteur = auteur
 
-    def toon_info(self):
-        return "ID: {} - Titel: {} - Auteur: {}".format(self.id, self.titel, self.auteur)
+    def __str__(self):
+        return "ID: {} - Titel: {} - Auteur: {} ".format(self.id, self.naam, self.auteur)
 
-    def toon_titel(self):
-        return self.titel
-
-    def toon_auteur(self):
-        return self.auteur
+    def toon_boek_auteur(self):
+        return "Titel: {} - Auteur: {}".format(self.naam, self.auteur)
 
 
 class Persoon:
-    def __init__(self, lid_nr, naam, geslacht):
-        self.lid_nr = lid_nr
+    def __init__(self, id, naam, geslacht):
+        self.id = id
         self.naam = naam
         self.geslacht = geslacht
 
-    def toon_info(self):
-        return "Lid Nr: {} - Naam: {} - Geslacht: {}".format(self.lid_nr, self.naam, self.geslacht)
-
-    def toon_lidnr(self):
-        return self.lid_nr
-
-    def toon_naam(self):
-        return self.naam
-
-    def toon_geslacht(self):
-        return self.geslacht
+    def __str__(self):
+        return "ID: {} - Naam: {} - Geslacht: {} ".format(self.id, self.naam, self.geslacht)
 
 
 class Uitlening:
@@ -39,8 +27,29 @@ class Uitlening:
         self.boek = boek
         self.persoon = persoon
 
-    def toon_info(self):
-        return "ID: {} - Boek: {} - Persoon: {}".format(self.id, self.boek.titel, self.persoon.naam)
+    def __str__(self):
+        return "ID {} - Boek: {} - Persoon: {}".format(self.id, self.boek.naam, self.persoon.naam)
+
+
+b1 = Boek("B1", "Harry Potter", "J.K. Rowling")
+b2 = Boek("B2", "Davinci Code", "Dan Brown")
+b3 = Boek("B3", "LOTR", "JRR Tolkien")
+b4 = Boek("B4", "GoT", "George RR Martin")
+
+boeken = [b1, b2, b3, b4]
+
+p1 = Persoon("P1", "Geert", "man")
+p2 = Persoon("P2", "Elke", "vrouw")
+p3 = Persoon("P3", "Sofie", "vrouw")
+p4 = Persoon("P4", "Peter", "man")
+
+personen = [p1, p2, p3, p4]
+
+u1 = Uitlening("U1", b1, p3)
+u2 = Uitlening("U2", b3, p2)
+u3 = Uitlening("U3", b4, p1)
+
+uitleningen = [u1, u2, u3]
 
 
 def toon_menu():
@@ -56,104 +65,107 @@ def toon_menu():
     print("10: Verwijder uitlening")
 
 
-def toon_alle_boeken(lijst):
-    for x in lijst:
-        print(x.toon_info())
+def toon_boeken():
+    for x in boeken:
+        print(x)
 
 
-def toon_alle_personen(lijst):
-    for x in lijst:
-        print(x.toon_info())
+def toon_personen():
+    for x in personen:
+        print(x)
 
 
-def toon_alle_uitleningen(lijst):
-    for x in lijst:
-        print(x.toon_info())
+def toon_uitleningen():
+    for x in uitleningen:
+        print(x)
 
 
-def boek_toevoegen(lijst):
-    id = input("Geef het boek ID in ")
-    titel = input("Geef de titel van het boek in ")
-    auteur = input("Geef de auteur van het boek in ")
-    b = Boek(id, titel, auteur)
-    lijst.append(b)
+def voeg_boek_toe():
+    id = input("Geef het ID")
+    naam = input("Geef de naam")
+    auteur = input("Geef de auteur")
+    b = Boek(id, naam, auteur)
+    boeken.append(b)
 
 
-def persoon_toevoegen(lijst):
-    lid_nr = input("Geef het lid nr in ")
-    naam = input("Geef de naam van de persoon in ")
-    geslacht = input("Geef het geslacht van de persoon in (M/V) ")
-    p = Persoon(lid_nr, naam, geslacht)
-    lijst.append(p)
+def voeg_persoon_toe():
+    id = input("Geef het ID")
+    naam = input("Geef de naam")
+    geslacht = input("Geef het geslacht man/vrouw/x")
+    p = Persoon(id, naam, geslacht)
+    personen.append(p)
 
 
-def nieuwe_uitlening(lijstB, lijstP, lijstU):
-    id = input("Geef het ID van de uitlening in ")
-    id_p = input("Geef het ID van de persoon ")
-    for x in lijstP:
-        if id_p == x.toon_lidnr():
-            persoon_naam = x.toon_naam()
-            persoon_geslacht = x.toon_geslacht()
-            p = Persoon(id_p, persoon_naam, persoon_geslacht)
-    id_b = input("Geef het ID van het boek in ")
-    for y in lijstB:
-        if id_b == y.id:
-            boek_naam = y.toon_titel()
-            boek_auteur = y.toon_auteur()
-            b = Boek(id_b, boek_naam, boek_auteur)
-    u = Uitlening(id, b, p)
-    lijstU.append(u)
+def voeg_uitlening_toe():
+    id = input("geef het ID")
+    id_boek = input("geef het id van het boek")
+    # bestaat het boek
+    for x in boeken:
+        if id_boek == x.id:
+            b = Boek(id_boek, x.naam, x.auteur)
+
+    id_persoon = input("geef het id van de persoon")
+    for y in personen:
+        if id_persoon == y.id:
+            p = Persoon(id_persoon, y.naam, y.geslacht)
+
+    if isinstance(b, Boek) and isinstance(p, Persoon):
+        u = Uitlening(id, b, p)
+        uitleningen.append(u)
+    else:
+        print("Uitlening kan niet worden toegevoegd")
 
 
-def sorteer_boeken(lijst):
-    lijst.sort(key=lambda x: x.titel)
-    for x in lijst:
-        print(x.toon_info())
+def toon_uitleningen_boek():
+    id_boek = input("Geef het id van het boek")
+    for x in uitleningen:
+        if id_boek == x.boek.id:
+            print(x.persoon)
+        else:
+            print("Boek bestaat niet of nog niet uitgeleend")
 
 
-b1 = Boek("B1", "Harry Potter", "J. K. Rowling")
-b2 = Boek("B2", "Davinci Code", "Dan Brown")
-b3 = Boek("B3", "LOTR", "JRR Tolkien")
-b4 = Boek("B4", "GoT", "George RR Martin")
+def toon_boek_geslacht():
+    geslacht = input("geef het gelacht")
+    for x in uitleningen:
+        if geslacht == x.persoon.geslacht:
+            print(x.boek)
 
-boeken = [b1, b2, b3, b4]
 
-p1 = Persoon("P1", "Geert", "M")
-p2 = Persoon("P2", "Elke", "V")
-p3 = Persoon("P3", "Sofie", "V")
-p4 = Persoon("P4", "Peter", "M")
+def sorteer_boek_AZ():
+    boeken.sort(key=lambda x: x.naam)
+    for x in boeken:
+        print(x)
 
-personen = [p1, p2, p3, p4]
 
-u1 = Uitlening("U1", b1, p3)
-u2 = Uitlening("U2", b3, p2)
-u3 = Uitlening("U3", b4, p1)
+def verwijder_uitlening():
+    id = input("geef het id van de uitlening")
+    for x, o in enumerate(uitleningen):
+        if o.id == id:
+            uitleningen.pop(x)
 
-uitleningen = [u1, u2, u3]
 
 # hoofdprogramma
-toon_menu()
-print()
-keuze = input("geef je keuze in ")
-while not keuze == "stop":
-    if keuze == "1":
-        toon_alle_boeken(boeken)
-    elif keuze == "2":
-        toon_alle_uitleningen(uitleningen)
-    elif keuze == "3":
-        toon_alle_personen(personen)
-    elif keuze == "4":
-        boek_toevoegen(boeken)
-    elif keuze == "5":
-        persoon_toevoegen(personen)
-    elif keuze == "6":
-        nieuwe_uitlening(boeken, personen, uitleningen)
-    elif keuze == "9":
-        sorteer_boeken(boeken)
-
-    print()
-    print("-----------------------------------")
-    print()
+while True:
     toon_menu()
-    print()
-    keuze = input("geef je keuze in ")
+    keuze = input("geef een keuze ")
+    if keuze == "1":
+        toon_boeken()
+    elif keuze == "2":
+        toon_personen()
+    elif keuze == "3":
+        toon_uitleningen()
+    elif keuze == "4":
+        voeg_boek_toe()
+    elif keuze == "5":
+        voeg_persoon_toe()
+    elif keuze == "6":
+        voeg_uitlening_toe()
+    elif keuze == "7":
+        toon_uitleningen_boek()
+    elif keuze == "8":
+        toon_boek_geslacht()
+    elif keuze == "9":
+        sorteer_boek_AZ()
+    elif keuze == "10":
+        verwijder_uitlening()
