dfd0fa992cf6705a14d701950e24205ef851e4fd
JBelgian <jordy.hanno@gmail.com>
2022-03-21 16:03:28 +0100

    Commit

--- Module 3/Les 5/Quiz/Quiz.py
+++ Module 3/Les 5/Quiz/Quiz.py
@@ -1,211 +1,187 @@
 # Python program to create a simple GUI
 # Simple Quiz using Tkinter
 
-# import everything from tkinter
+# import alles van tkinter
 from tkinter import *
 
-# and import messagebox as mb from tkinter
+# import messagebox als mb van tkinter
 from tkinter import messagebox as mb
 
-# import json to use json file for data
+# import json om json file te gebruiken voor data
 import json
 
 
-# class to define the components of the GUI
+# class om de components van de GUI te defineren
 class Quiz:
-    # This is the first method which is called when a
-    # new object of the class is initialized. This method
-    # sets the question count to 0. and initialize all the
-    # other methoods to display the content and make all the
-    # functionalities available
     def __init__(self):
 
-        # set question number to 0
+        # zet question number naar 0
         self.q_no = 0
 
-        # assigns ques to the display_question function to update later.
+        # functie voor het weergeven van de titel in titel balk en de vraag
         self.display_title()
         self.display_question()
 
-        # opt_selected holds an integer value which is used for
-        # selected option in a question.
+        # opt_selected houd een int vast die correspondeert met het gekoze antwoord
         self.opt_selected = IntVar()
 
-        # displaying radio button for the current question and used to
-        # display options for the current question
+        # weergeeft radio buttons voor de antwoord opties
         self.opts = self.radio_buttons()
 
-        # display options for the current question
+        # weergeeft de antwoord opties voor de huidige vraag
         self.display_options()
 
-        # displays the button for next and exit.
+        # weergeeft de knoppen next en quit
         self.buttons()
 
-        # no of questions
+        # berekent het aantal vragen in de quiz
         self.data_size = len(question)
 
-        # keep a counter of correct answers
+        # houd score van juiste antwoorden bij
         self.correct = 0
 
-    # This method is used to display the result
-    # It counts the number of correct and wrong answers
-    # and then display them at the end as a message Box
+    # functie berekent correct en niet correct antwoord en weergeeft een message box met resultaat
     def display_result(self):
 
-        # calculates the wrong count
+        # berekent foute antwoorden
         wrong_count = self.data_size - self.correct
         correct = f"Correct: {self.correct}"
         wrong = f"Wrong: {wrong_count}"
 
-        # calcultaes the percentage of correct answers
+        # berekent percentage juist geantwoord
         score = int(self.correct / self.data_size * 100)
         result = f"Score: {score}%"
 
-        # Shows a message box to display the result
+        # toont message box met resultaten
         mb.showinfo("Result", f"{result}\n{correct}\n{wrong}")
 
-    # This method checks the Answer after we click on Next.
+    # functie checkt antwoord wanneer op next wordt geduwt
     def check_ans(self, q_no):
 
-        # checks for if the selected option is correct
+        # check of the geselecteerde antwoord juist is
         if self.opt_selected.get() == answer[q_no]:
-            # if the option is correct it return true
+            # als het juist is returned de functie true
             return True
 
-    # This method is used to check the answer of the
-    # current question by calling the check_ans and question no.
-    # if the question is correct it increases the count by 1
-    # and then increase the question number by 1. If it is last
-    # question then it calls display result to show the message box.
-    # otherwise, shows next question.
+    # functie checkt of the geselecteerd antwoord juist is voor de huidige vraag
+    # indien juist telt hij +1 bij correct antwoord teller
+    # en gaat hij naar de volgende vraag, tenzij het de laatste vraag is
+    # dan opent de functie het resultaten message box
     def next_btn(self):
 
-        # Check if the answer is correct
+        # Checkt of het antwoord juist is
         if self.check_ans(self.q_no):
-            # if the answer is correct it increments the correct by 1
+            # als het antwoord juist is +1 op correcte teller
             self.correct += 1
 
-        # Moves to next Question by incrementing the q_no counter
+        # gaat naar volgende vraag
         self.q_no += 1
 
-        # checks if the q_no size is equal to the data size
+        # checkt als de huidige vraag overeenkomt met het totaal aantal vragen in de quiz
         if self.q_no == self.data_size:
 
-            # if it is correct then it displays the score
+            # als huidige vraag de laatste vraag is open het resultaat message box
             self.display_result()
 
-            # destroys the GUI
+            # sluit de quiz
             gui.destroy()
         else:
-            # shows the next question
+            # anders toont de functie de volgende vraag en antwoorden
             self.display_question()
             self.display_options()
 
-    # This method shows the two buttons on the screen.
-    # The first one is the next_button which moves to next question
-    # It has properties like what text it shows the functionality,
-    # size, color, and property of text displayed on button. Then it
-    # mentions where to place the button on the screen. The second
-    # button is the exit button which is used to close the GUI without
-    # completing the quiz.
+    # deze functie toont de twee knoppen
+    # en zorgt ervoor de bij het klikken op de knop de juiste functie aangeroepen wordt
     def buttons(self):
 
-        # The first button is the Next button to move to the
-        # next Question
+        # eerste knop is de next knop, roept de volgende vraag of resultaat op
         next_button = Button(gui, text="Next", command=self.next_btn,
                              width=10, bg="blue", fg="white", font=("ariel", 16, "bold"))
 
-        # palcing the button on the screen
+        # plaatsing van de next knop in het screen
         next_button.place(x=350, y=380)
 
-        # This is the second button which is used to Quit the GUI
+        # de knop is de quit knop om het programma af te sluiten
         quit_button = Button(gui, text="Quit", command=gui.destroy,
                              width=5, bg="black", fg="white", font=("ariel", 16, " bold"))
 
-        # placing the Quit button on the screen
+        # plaatsing van de quit knop in het screen
         quit_button.place(x=700, y=50)
 
-    # This method deselect the radio button on the screen
-    # Then it is used to display the options available for the current
-    # question which we obtain through the question number and Updates
-    # each of the options for the current question of the radio button.
+    # deze functie zorgt ervoor de antwoorden bij de huidige vraag worden weergeven
     def display_options(self):
         val = 0
 
-        # deselecting the options
+        # deselecting van de opties
         self.opt_selected.set(0)
 
-        # looping over the options to be displayed for the
-        # text of the radio buttons.
+        # looping voor het weergeven van de verschillende antwoord mogelijkheden bij iedere radio button
         for option in options[self.q_no]:
             self.opts[val]['text'] = option
             val += 1
 
-    # This method shows the current Question on the screen
+    # functie voor het aanroepen en weergeven van de vraag
     def display_question(self):
 
-        # setting the Question properties
+        # label properties (kleur, lettertijpe, grootte) en aanroepen vraag
         q_no = Label(gui, text=question[self.q_no], width=60,
                      font=('ariel', 16, 'bold'), anchor='w')
 
-        # placing the option on the screen
+        # plaatsing van de vraag in het screen
         q_no.place(x=70, y=100)
 
-    # This method is used to Display Title
+    # functie om titel te weergeven
     def display_title(self):
 
-        # The title to be shown
-        title = Label(gui, text="GeeksforGeeks QUIZ",
+        # titel label properties (kleur, lettertijpe, grootte)
+        title = Label(gui, text="DAJ QUIZ",
                       width=50, bg="green", fg="white", font=("ariel", 20, "bold"))
 
-        # place of the title
+        # plaatsing van de titel in het screen
         title.place(x=0, y=2)
 
-    # This method shows the radio buttons to select the Question
-    # on the screen at the specified position. It also returns a
-    # list of radio button which are later used to add the options to
-    # them.
+    # functie om de radio buttons te displayen
     def radio_buttons(self):
 
-        # initialize the list with an empty list of options
+        # initieel een lege versie aangemaakt
         q_list = []
 
-        # position of the first option
+        # positie van de eerste knop
         y_pos = 150
 
-        # adding the options to the list
+        # antwoord opties aan de lijst toevoegen
         while len(q_list) < 4:
-            # setting the radio button properties
+            # radio button properties
             radio_btn = Radiobutton(gui, text=" ", variable=self.opt_selected,
                                     value=len(q_list) + 1, font=("ariel", 14))
 
-            # adding the button to the list
+            # toevoegen van de radio buttons aan de lijst
             q_list.append(radio_btn)
 
-            # placing the button
+            # plaatsing van de buttons
             radio_btn.place(x=100, y=y_pos)
 
-            # incrementing the y-axis position by 40
+            # incrementeren op de y-as positie met 40
             y_pos += 40
 
-        # return the radio buttons
+        # lijst terug koppene naar hoofd programma
         return q_list
 
 
-# Create a GUI Window
+# Creeert GUI Window
 gui = Tk()
 
-# set the size of the GUI Window
+# zet de groote van GUI Window
 gui.geometry("800x450")
 
-# set the title of the Window
-gui.title("GeeksforGeeks Quiz")
+# zet de titel van de GUI Window
+gui.title("DAJ Quiz")
 
-# get the data from the json file
+# haalt de data van de json file
 with open('data.json') as f:
     data = json.load(f)
 
-# set the question, options, and answer
+# zet de vraag, opties en juist antwoord
 question = (data['question'])
 options = (data['options'])
 answer = (data['answer'])
@@ -213,7 +189,6 @@ answer = (data['answer'])
 # create an object of the Quiz Class.
 quiz = Quiz()
 
-# Start the GUI
-gui.mainloop()
-
+# start het programma
+mainloop()
 # END OF THE PROGRAM
